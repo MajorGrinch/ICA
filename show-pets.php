07b280bcf726a4f714f899c964dc723bb92368d5
Kevin <majorgrinch@gmail.com>
2017-10-11 11:28:44 -0500

    step 6 finished

--- show-pets.php
+++ show-pets.php
@@ -7,6 +7,10 @@ if (!isset($_SESSION['user'])) {
 ?>
 
 <!DOCTYPE html>
+<?php
+session_start();
+require 'database.php';
+?>
 <html>
 <head>
 	<title>All pets</title>
@@ -14,8 +18,24 @@ if (!isset($_SESSION['user'])) {
 <body>
 <h1>All pets</h1>
 <?php
+        $stmt = $mysqli->prepare("select username, species, petname, weight, description, picture from pets");
 
+        if (!$stmt) {
+            printf("Query Prep Failed: %s\n", $mysqli->error);
+            exit;
+        }
+        $stmt->execute();
+        $result = $stmt->bind_resut($username, $species, $petname, $weight, $description, $picture);
+        while( $result->fetch()){
 ?>
+<img src="<?php echo $picture?>"/>
+<div class="pet_item">
+	<h2><?php echo $petname ?></h2>
+	<p><bold><?php echo $username ?></bold></p>
+	<p><?php echo $weight ?></p>
+	<p><?php echo $species?></p>
+	<p><?php echo $description ?></p>
+</div>
 </form>
 </body>
 </html>
